5bbdf3f5a05aa0a76ca1f9afa704e6c3c87f8919
delayedaa <delayedaa@gmail.com>
2021-01-11 10:03:18 -0600

    Complete problems 1-6

--- fib.py
+++ fib.py
@@ -6,16 +6,20 @@
 # For this function, the first two fibonacci numbers are 1 and 1
 
 def fib(n):
-    # Write code here
-    pass
+    if n < 1:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 2) + fib(n - 1)
 
-# print(fib(-1))
+
+print(fib(-1))
 # => 0
-# print(fib(0))
+print(fib(0))
 # => 0
-# print(fib(1))
+print(fib(1))
 # => 1
-# print(fib(2))
+print(fib(2))
 # => 1
-# print(fib(7))
+print(fib(7))
 # => 13
--- find_max.py
+++ find_max.py
@@ -3,9 +3,15 @@
 
 # This function returns the largest number in a given array.
 
-def find_max(l):
-    # Write code here
-    pass
+def find_max(l, largest_num=-float('inf')):
+    if len(l) == 0:
+        return largest_num
+    if l[0] > largest_num:
+        new_largest_num = l[0]
+    else:
+        new_largest_num = largest_num
+    return find_max(l[1:], new_largest_num)
 
-# print(find_max([1, 4, 45, 6, -50, 10, 2]))
+
+print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
